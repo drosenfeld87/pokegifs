e036d72e82e700a5391ff0a40f06f2338c9831a2
debbie <drosenfeld87@gmail.com>
2018-03-19 16:39:37 -0400

    complete assignment

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,17 +1,18 @@
 class PokemonController < ApplicationController
   def show
-    res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
-    body = JSON.parse(res.body)
-    name  = body["name"]
-    id = body["id"]
-    type = body["types"][0]["type"]["name"]
-
-    render json: { "id": id, "name": name, "type": type}
-
-    res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=pikachu&rating=g")
-    body = JSON.parse(res.body)
+     res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
+     body = JSON.parse(res.body)
+     id    = body["id"]
+     name  = body["name"]
+     type = body["types"][0]["type"]["name"]
 
 
-  end
-
+     result = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=#{name}&rating=g")
+     body_gyphy = JSON.parse(result.body)
+     gif_url = body_gyphy["data"][0]["url"]
+     render json: { "id": id,
+                   "name": name,
+                   "type": type,
+                   "url": gif_url}
+   end
 end
