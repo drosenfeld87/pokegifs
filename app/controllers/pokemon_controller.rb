506ebc72f7e91d21cefc7634d01acf19ded41be4
debbie <drosenfeld87@gmail.com>
2018-03-19 16:04:08 -0400

    JSON name, id and type are displaying

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,6 +1,17 @@
 class PokemonController < ApplicationController
   def show
-    render json: { "message": "ok" }
+    res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
+    body = JSON.parse(res.body)
+    name  = body["name"]
+    id = body["id"]
+    type = body["types"][0]["type"]["name"]
+
+    render json: { "id": id, "name": name, "type": type}
+
+    res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=pikachu&rating=g")
+    body = JSON.parse(res.body)
+
+
   end
 
 end
